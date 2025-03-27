a1aa0483991f5d476fcd4278bfd91eff3c1c864c
BjornW <bjorn.wedbratt@gmail.com>
2025-03-27 11:53:47 +0100

    installed uv for package management

--- docker_new/init.sh
+++ docker_new/init.sh
@@ -38,7 +38,7 @@ cd /comfyui
 
 # Install dependencies.
 # As this might change on new commits, it cannot be done during image build
-pip install -r requirements.txt --no-cache-dir 
+uv pip install -r requirements.txt --no-cache-dir 
 
 # Install xformers may not be required, and if so install in image
 # /comfyui/venv/bin/python -m pip install xformers
